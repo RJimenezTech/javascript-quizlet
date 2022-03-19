557468eaef0e9dfc9045487cb4870ccf62b36011
Robert Jimenez <robertjimenez86@gmail.com>
2022-03-18 23:25:11 -0500

    add header with button and footer to index, and add basic css

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+    // functions that control timer
+    // functions for presenting questions
+
+// WHEN I answer a question
+// THEN I am presented with another question
+    // functions for presenting questions
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+    // functions that control timer
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+    // functions that control flow of game
+    
+// WHEN the game is over
+// THEN I can save my initials and score
+    // functions that control flow of game
+    // functions that store score
+    // functions that allow for input of initials
