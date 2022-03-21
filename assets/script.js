3a394c7cd7cc5a6825a7a2b7f80661f13ac0748f
Robert Jimenez <robertjimenez86@gmail.com>
2022-03-20 20:19:49 -0500

    reworked generateQeustionCard function to only updat HTML text dynamically, not generate entire card element, also added outcome response throughout

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,13 @@ let quizTime = "";
 // create area where questions will display
 let questionAreaEl = document.querySelector("#question-area");
 let mainArea = document.querySelector("#page-content");
-// let questionAnswerEl = document.querySelector("#question-answer-container")
-let submitButton = document.querySelectorAll("#answer");
+// find buttons for answer click events
+let answerButtonElA = document.querySelector(".question-A");
+let answerButtonElB = document.querySelector(".question-B");
+let answerButtonElC = document.querySelector(".question-C");
+let answerButtonElD = document.querySelector(".question-D");
+// find the question outcome section
+let outcomeEl = document.querySelector(".outcome");
 // current question number
 let questionNumber = 0;
 // high score handling variables
@@ -36,14 +41,164 @@ let questionObjectArr = [
     answerB: "TypeScript",
     answerC: "JavaScript", 
     answerD: "JabbaScript", 
+    correctAnswer: "answerC"},
+    {questionID: "0",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "JavaScript", 
+    correctAnswer: "answerD"},
+    {questionID: "1",
+    questionText: "How do you spell JavaScript?",
+    answerA: "JavaScript",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "Java", 
+    correctAnswer: "answerA"},
+    {questionID: "2",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JavaScript", 
+    answerD: "JabbaScript", 
+    correctAnswer: "answerC"},
+    {questionID: "3",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "JavaScript", 
+    correctAnswer: "answerD"},
+    {questionID: "4",
+    questionText: "How do you spell JavaScript?",
+    answerA: "JavaScript",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "Java", 
+    correctAnswer: "answerA"},
+    {questionID: "5",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JavaScript", 
+    answerD: "JabbaScript", 
+    correctAnswer: "answerC"},
+    {questionID: "6",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "JavaScript", 
+    correctAnswer: "answerD"},
+    {questionID: "7",
+    questionText: "How do you spell JavaScript?",
+    answerA: "JavaScript",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "Java", 
+    correctAnswer: "answerA"},
+    {questionID: "8",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JavaScript", 
+    answerD: "JabbaScript", 
+    correctAnswer: "answerC"},
+    {questionID: "9",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "JavaScript", 
+    correctAnswer: "answerD"},
+    {questionID: "10",
+    questionText: "How do you spell JavaScript?",
+    answerA: "JavaScript",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "Java", 
+    correctAnswer: "answerA"},
+    {questionID: "11",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JavaScript", 
+    answerD: "JabbaScript", 
+    correctAnswer: "answerC"},
+    {questionID: "12",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "JavaScript", 
+    correctAnswer: "answerD"},
+    {questionID: "13",
+    questionText: "How do you spell JavaScript?",
+    answerA: "JavaScript",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "Java", 
+    correctAnswer: "answerA"},
+    {questionID: "14",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JavaScript", 
+    answerD: "JabbaScript", 
+    correctAnswer: "answerC"},
+    {questionID: "15",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "JavaScript", 
+    correctAnswer: "answerD"},
+    {questionID: "16",
+    questionText: "How do you spell JavaScript?",
+    answerA: "JavaScript",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "Java", 
+    correctAnswer: "answerA"},
+    {questionID: "17",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JavaScript", 
+    answerD: "JabbaScript", 
+    correctAnswer: "answerC"},
+    {questionID: "18",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "JavaScript", 
+    correctAnswer: "answerD"},
+    {questionID: "19",
+    questionText: "How do you spell JavaScript?",
+    answerA: "JavaScript",
+    answerB: "TypeScript",
+    answerC: "JabbaScript", 
+    answerD: "Java", 
+    correctAnswer: "answerA"},
+    {questionID: "20",
+    questionText: "How do you spell JavaScript?",
+    answerA: "Java",
+    answerB: "TypeScript",
+    answerC: "JavaScript", 
+    answerD: "JabbaScript", 
     correctAnswer: "answerC"}
 ];
+let randomNumber = function() {
+    return Math.floor(Math.random() * questionObjectArr.length + 1);
+}
 // function to decrease time
 let decrementTimer = function(num) {
     quizTime = quizTime - num;
 };
 // function to set a timer output to current value of quiztime to the timer element every second
-let startButtonHandler = function() {  
+let timerHandler = function() {  
     // set quizTime to 59 to eliminate time delay
     quizTime = 59;
     var timerOutput = setInterval(function() {
@@ -63,110 +218,78 @@ let startButtonHandler = function() {
     }, 1 * 1000 )
 };
 let gameHandler = function() {
-    // once timer has started, remove "ready?" text
-    var readyInfoEl1 = document.querySelector("#ready-info1");
-    var readyInfoEl2 = document.querySelector("#ready-info2");  
-    if (quizTime <= 59) {
-            readyInfoEl1.textContent = "";
-            readyInfoEl2.textContent = "";
+    // remove "Press START QUIZ to begin" text
+    var removeTextEl = document.querySelector(".question-text");
+    removeTextEl.textContent = "";
+    
+    // first question at start of game
+    generateQuestionCard();
+    if (checkAnswer) {
+        generateQuestionCard();
+        outcomeEl.textContent = "correct!";
+    } else {
+        outcomeEl.textContent = "wrong!";
     }
     
-    var newQuestionCard = generateQuestionCard(questionObjectArr[questionNumber]);
-    questionAreaEl.appendChild(newQuestionCard);  
 
 }
-let generateQuestionCard = function(questionObject) {
-    // create elements in a question card
-    var questionCardEl = document.createElement("div");
-    var questionTextEl = document.createElement("h1");
-    var answerContainerEl = document.createElement("div");
-    var answerA = document.createElement("button");
-    answerA.addEventListener("click", checkAnswer);
-    var answerB = document.createElement("button");
-    answerB.addEventListener("click", checkAnswer);
-    var answerC = document.createElement("button");
-    answerC.addEventListener("click", checkAnswer);
-    var answerD = document.createElement("button");
-    answerD.addEventListener("click", checkAnswer);
 
-    // assign classes and question IDs to each element
-    questionCardEl.className = "question-card";
-    questionCardEl.setAttribute("data-id", questionObject.questionID)
-    questionTextEl.className = "question-text";
-    questionTextEl.setAttribute("data-id", questionObject.questionID)
-    answerContainerEl.className = "question-answer-container";
-    answerContainerEl.setAttribute("data-id", questionObject.questionID)
-    answerA.className = "question-A";
-    answerA.setAttribute("id", "answer");
-    answerA.setAttribute("data-id", questionObject.questionID);
-    answerA.setAttribute("data-whichAnswer", "answerA")
-    answerB.className = "question-B";
-    answerB.setAttribute("id", "answer");
-    answerB.setAttribute("data-id", questionObject.questionID);
-    answerB.setAttribute("data-whichAnswer", "answerB")
-    answerC.className = "question-C"
-    answerC.setAttribute("id", "answer");
-    answerC.setAttribute("data-id", questionObject.questionID);
-    answerC.setAttribute("data-whichAnswer", "answerC")
-    answerD.className = "question-D"
-    answerD.setAttribute("id", "answer");
-    answerD.setAttribute("data-id", questionObject.questionID);
-    answerD.setAttribute("data-whichAnswer", "answerD")
+// function that dynamically updates the text for each question
+let generateQuestionCard = function() {
+    // find a random question in the question array
+    var currentQuestionObj = questionObjectArr[randomNumber()];
+    // create elements in a question card and update the text of each element 
+    // to reflect the currect random question
+    var questionTextEl = document.querySelector(".question-text")
+    questionTextEl.textContent = currentQuestionObj.questionText;
+    var answerAEl = document.querySelector(".question-A");
+    answerAEl.textContent = currentQuestionObj.answerA;
+    var answerBEl = document.querySelector(".question-B");
+    answerBEl.textContent = currentQuestionObj.answerB;
+    var answerCEl = document.querySelector(".question-C");
+    answerCEl.textContent = currentQuestionObj.answerC;
+    var answerDEl = document.querySelector(".question-D");
+    answerDEl.textContent = currentQuestionObj.answerD;
+
     
-
-    // assign information to each element
-    questionTextEl.textContent = questionObject.questionText + " " + questionObject.questionID;
-    answerA.textContent = questionObject.answerA;
-    answerB.textContent = questionObject.answerB;
-    answerC.textContent = questionObject.answerC;
-    answerD.textContent = questionObject.answerD;
-
-    // add elements to card
-    questionCardEl.appendChild(questionTextEl);
-    answerContainerEl.appendChild(answerA);
-    answerContainerEl.appendChild(answerB);
-    answerContainerEl.appendChild(answerC);
-    answerContainerEl.appendChild(answerD);
-    questionCardEl.appendChild(answerContainerEl);
-
-    return questionCardEl;
+ 
 }
 // check if the answer was correct
 let checkAnswer = function (event) {
-    let answerClicked = event.target;
-    workingID = answerClicked.getAttribute("data-id");
-    let currentQuestionObj = {};
-    // find the question object in in the array for comparison
-    for (let i = 0; i < questionObjectArr.length; i++) {
-        if (questionObjectArr[i].questionID === workingID) {
-            currentQuestionObj = questionObjectArr[i];
-        }
-    }
-    // if the current question's answer was clicked,
-    // the user got this question correct
-    if (currentQuestionObj.correctAnswer === answerClicked.getAttribute("data-whichAnswer")) {
-        // if correct remove current question card
-        score++;
-        console.log("score is " + score);
-        var removeEl = document.querySelector(".question-card[data-id='" + workingID +"']");
-        console.log(removeEl)
-        removeEl.remove();
-        // move to next question
-        questionNumber++;
-        // as long as there are questions left in the array
-        if (questionNumber < questionObjectArr.length) {
-            // create a new question card with the next question object
-            newQuestionCard = generateQuestionCard(questionObjectArr[questionNumber]);
-            questionAreaEl.appendChild(newQuestionCard);
-        } else { // timer goes to zero (dummy action)
-            quizTime = 0;
-        }
-    }
-    else {
-        // console.log("you chose the wrong answer")
-        decrementTimer(4);
-    }
-    // this.parentNode.parentNode.remove();
+//     let answerClicked = event.target;
+//     workingID = answerClicked.getAttribute("data-id");
+//     let currentQuestionObj = {};
+//     // find the question object in in the array for comparison
+//     for (let i = 0; i < questionObjectArr.length; i++) {
+//         if (questionObjectArr[i].questionID === workingID) {
+//             currentQuestionObj = questionObjectArr[i];
+//         }
+//     }
+//     // if the current question's answer was clicked,
+//     // the user got this question correct
+//     if (currentQuestionObj.correctAnswer === answerClicked.getAttribute("data-whichAnswer")) {
+//         // if correct remove current question card
+//         score++;
+//         console.log("score is " + score);
+//         // var removeEl = document.querySelector(".question-card[data-id='" + workingID +"']");
+//         // console.log(removeEl)
+//         // removeEl.remove();
+//         // move to next question
+//         questionNumber++;
+//         // as long as there are questions left in the array
+//         if (questionNumber < questionObjectArr.length) {
+//             // create a new question card with the next question object
+//             newQuestionCard = generateQuestionCard(questionObjectArr[questionNumber]);
+//             questionAreaEl.appendChild(newQuestionCard);
+//         } else { // timer goes to zero (dummy action)
+//             quizTime = 0;
+//         }
+//     }
+//     else {
+//         // console.log("you chose the wrong answer")
+//         decrementTimer(4);
+//     }
+//     // this.parentNode.parentNode.remove();
 }
 
 let saveScore = function() {
@@ -209,6 +332,11 @@ let endGame = function() {
 
 
 // wait for click on start button to start timer
-startQuizEl.addEventListener("click", startButtonHandler);
+startQuizEl.addEventListener("click", timerHandler);
 // wait for click on start button to start game
 startQuizEl.addEventListener("click", gameHandler);
+// wait for click on answer button
+answerButtonElA.addEventListener("click", checkAnswer);
+answerButtonElB.addEventListener("click", checkAnswer);
+answerButtonElC.addEventListener("click", checkAnswer);
+answerButtonElD.addEventListener("click", checkAnswer);
