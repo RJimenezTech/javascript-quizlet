b09e0e77bafa25abc8a492862c1659f19b33359a
Robert Jimenez <robertjimenez86@gmail.com>
2022-03-22 18:14:20 -0500

    added end game process to prompt for initials for high score, and save high score with initials

--- assets/script.js
+++ assets/script.js
@@ -18,30 +18,9 @@ let outcomeEl = document.querySelector(".outcome");
 let questionNumber = 0;
 // high score handling variables
 let score = 0;
-let highScoreArr = [];
+let highScore = 0;
 // question object array(format is {questionID, questionText, answer1, answer2, answer3, answer4, correntAnswer})
 let questionObjectArr = [
-    {questionID: "0",
-    questionText: "How do you spell JavaScript?",
-    answerA: "Java",
-    answerB: "TypeScript",
-    answerC: "JabbaScript", 
-    answerD: "JavaScript", 
-    correctAnswer: "answerD"},
-    {questionID: "1",
-    questionText: "How do you spell JavaScript?",
-    answerA: "JavaScript",
-    answerB: "TypeScript",
-    answerC: "JabbaScript", 
-    answerD: "Java", 
-    correctAnswer: "answerA"},
-    {questionID: "2",
-    questionText: "How do you spell JavaScript?",
-    answerA: "Java",
-    answerB: "TypeScript",
-    answerC: "JavaScript", 
-    answerD: "JabbaScript", 
-    correctAnswer: "answerC"},
     {questionID: "0",
     questionText: "How do you spell JavaScript?",
     answerA: "Java",
@@ -205,13 +184,14 @@ let timerHandler = function() {
     var removeTextEl = document.querySelector(".question-text");
     removeTextEl.textContent = "";
     // set quizTime to 59 to eliminate time delay
-    quizTime = 59;
+    quizTime = 2;
     var timerOutput = setInterval(function() {
         // check if timer is negative
-        if (quizTime === 0) {
+        if (quizTime <= 0) {
             // if yes, remove timer
             timerEl.textContent = "Time : 00 s";
             clearInterval(timerOutput);
+            endGame();
         } else {
             // otherwise update content in timerEl with the current quizTime
             timerEl.textContent = "Time : " + quizTime + " s";
@@ -249,7 +229,7 @@ let gameHandler = function() {
     answerCEl.setAttribute("data-question-id", currentQuestionObj.questionID);
     answerCEl.textContent = currentQuestionObj.answerC;
     answerDEl.setAttribute("data-question-id", currentQuestionObj.questionID);
-    answerDEl.textContent = currentQuestionObj.answerD;   
+    answerDEl.textContent = currentQuestionObj.answerD;       
 }
 
 // function that dynamically updates the text for each question
@@ -280,9 +260,13 @@ let generateQuestionCard = function(event) {
         answerCEl.setAttribute("data-question-id", currentQuestionObj.questionID);
         answerCEl.textContent = currentQuestionObj.answerC;
         answerDEl.setAttribute("data-question-id", currentQuestionObj.questionID);
-        answerDEl.textContent = currentQuestionObj.answerD;    
+        answerDEl.textContent = currentQuestionObj.answerD;   
+        // update score as we get quesitons correct
+        score++;
+        console.log(score) ;
     } else {
-        decrementTimer(5);
+        // decrease timer as we get questions wrong
+        decrementTimer(4);
         return;
     }   
 }
@@ -292,7 +276,6 @@ let checkAnswer = function (event) {
     let answerClicked = event.target;
     console.log(answerClicked.getAttribute("data-whichAnswer"));
     console.log(currentQuestionObj.correctAnswer);
-    // let currentQuestionObj = {};
     if (answerClicked.getAttribute("data-whichAnswer") === currentQuestionObj.correctAnswer) {
         console.log("correct!")
         return true;
@@ -302,35 +285,97 @@ let checkAnswer = function (event) {
         return false;
     }
 }
+// let initialsHandler = function() {
+//     // wait for input area 
+//     var initialsVal = document.querySelector(".initials-input").value;
+//     if (!initialsVal > 2) {
+//         scoreHandler();
+//     } else {
+//         localStorage.setItem("initials",JSON.stringify(initialsVal));
+//         console.log(initialsVal)
+//     }
+// }   
 
+let scoreHandler = function() {
+    var questionTextEl = document.querySelector(".question-text")
+    // var questionContainerEl = document.querySelector(".question-answer-container")
+    var answerAEl = document.querySelector(".question-A");
+    var answerBEl = document.querySelector(".question-B");
+    var answerCEl = document.querySelector(".question-C");
+    var answerDEl = document.querySelector(".question-D");
+    // add infor for user about end game process
+    questionTextEl.textContent = "All done!";
+    questionTextEl.setAttribute("class","score-text");
+    answerAEl.remove();
+    answerBEl.remove();
+    answerCEl.remove(); 
+    answerDEl.remove(); 
+    // var inputInitialsEl = document.createElement("textarea");
+    // inputInitialsEl.className = "initials-input"
+    // var promptInitialEl = document.createElement("p")
+    // promptInitialEl.textContent = "Enter Initials for High Score!"
+    if (score >= highScore) {
+        // save new high score to localStorage
+        highScore = score;
+        localStorage.setItem("highScore", JSON.stringify(highScore));
+        // ask for initials via text and input area
+        var initials = window.prompt("enter your initials")
+        // questionTextEl.appendChild(promptInitialEl);
+        // questionTextEl.appendChild(inputInitialsEl);
+        // questionTextEl.addEventListener("submit", initialsHandler);
+        if (initials >= 2 || initials <= 0) {
+            initials = window.prompt("enter your initials");
+        } else {
+           localStorage.setItem("initials",JSON.stringify(initials));
+            console.log(initialsVal) 
+        }
+        
+    } else {
+        promptInitialEl.textContent = "Your score was not a High Score! Click OK to start over."
+    }
+}
 
-let saveScore = function() {
-    highScoreArr.push(score);
-    localStorage.setItem("highScore", JSON.stringify(highScoreArr));
+let clearQuestionCard = function() {
+    // function that removes all text from question card elements 
+    // only to be used at end of game
+    var questionTextEl = document.querySelector(".question-text")
+    var answerAEl = document.querySelector(".question-A");
+    var answerBEl = document.querySelector(".question-B");
+    var answerCEl = document.querySelector(".question-C");
+    var answerDEl = document.querySelector(".question-D");
+    questionTextEl.textContent = "GAME OVER!";
+    answerAEl.textContent = ""; 
+    answerBEl.textContent = ""; 
+    answerCEl.textContent = ""; 
+    answerDEl.textContent = ""; 
 }
 
 let endGame = function() {
-    saveScore();
+    // clear question
+    clearQuestionCard();
+    // save info after game is over
+    scoreHandler();
 }
+
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
-    // functions that control timer
+    // functions that control timer DONE
         // button element that updates time after start DONE
-        // function that reduces the timer when wrong 
+        // function that reduces the timer when wrong DONE
     // functions for presenting questions
         // array of question objects DONE
         // function that generates a new question card DONE
         // listen for click on answer buttons DONE
-        // check if answer was correct
-        // update DOM element that contains a question     
+        // check if answer was correct DONE
+        // update DOM element that contains a question DONE
 
 // WHEN I answer a question
 // THEN I am presented with another question
-    // functions for presenting questions
+    // functions for presenting questions DONE
 
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
-    // functions that control timer
+    // functions that control timer DONE
 
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
@@ -339,7 +384,7 @@ let endGame = function() {
 // WHEN the game is over
 // THEN I can save my initials and score
     // functions that control flow of game
-    // functions that store score
+    // functions that store score DONE
     // functions that allow for input of initials
 
 
